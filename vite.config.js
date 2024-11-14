4c14d0465d5d0661c0cdb8e25c7f47588dd90890
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-11-14 22:14:09 +0200

    structure & build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/echoapp-front/',
+  root: 'src',
   build: {
     rollupOptions: {
-      input: {
-        main: './index.html',
-        career: './public/pages/career.html',
-        contact: './public/pages/contact.html',
-        partnership: './public/pages/partnership.html',
-        prices: './public/pages/prices.html',
-        products: './public/pages/products.html',
-        team: './public/pages/team.html',
-      },
+      input: glob.sync('./src/*.html'),
     },
+    outDir: '../dist',
   },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
