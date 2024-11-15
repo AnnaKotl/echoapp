00be33c60387de1be328f1671780ef128a1992fc
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-11-15 15:34:31 +0200

    ROUTS & PAGES

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,25 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: '/echoapp-front/', 
+  // base: '/',
+  base: '/echoapp-front/',
   root: 'src',
   build: {
     rollupOptions: {
-      input: glob.sync('./src/index.html'),
+      input: {
+        main: './src/index.html',
+        career: './src/pages/career.html',
+        contacts: './src/pages/contacts.html',
+        partnership: './src/pages/partnership.html',
+        prices: './src/pages/prices.html',
+        products: './src/pages/products.html',
+        team: './src/pages/team.html',
+      },
     },
     outDir: '../dist',
     emptyOutDir: true,
   },
-  plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+  plugins: [
+    injectHTML(),
+  ],
 });
