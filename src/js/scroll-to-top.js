2fcf39d2947a9174e19b872b2dd518c2697236c6
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-11-23 17:16:39 +0200

    SCROLL done for Hero & Prices

--- src/js/scroll-to-top.js
+++ src/js/scroll-to-top.js
@@ -1,21 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const scrollToTopButton = document.getElementById("scrollToTop");
-  
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 1000) {
-        scrollToTopButton.classList.add("show");
-        scrollToTopButton.classList.remove("hide");
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollButton = document.getElementById('scroll-button');
+    
+    const toggleScrollButton = () => {
+      if (window.scrollY > 2000) {
+        scrollButton.classList.add('visible');
+        scrollButton.classList.remove('hidden');
       } else {
-        scrollToTopButton.classList.add("hide");
-        scrollToTopButton.classList.remove("show");
+        scrollButton.classList.add('hidden');
+        scrollButton.classList.remove('visible');
       }
-    });
+    };
   
-    scrollToTopButton.addEventListener("click", () => {
+    window.addEventListener('scroll', toggleScrollButton);
+  
+    scrollButton.addEventListener('click', () => {
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
+    });
+  
+    const scrollLink = document.querySelector('.hero-scroll-link');
+  
+    scrollLink.addEventListener('click', (event) => {
+      event.preventDefault();
+  
+      const targetSection = document.querySelector(scrollLink.getAttribute('href'));
+  
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth', 
+      });
+  
+      scrollLink.blur();
     });
   });
   
