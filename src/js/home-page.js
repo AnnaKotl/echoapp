2cb6e59efc39be46bd2107b614ea6f3d6681f3c5
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-11-30 16:49:45 +0200

    .running-line start

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,25 +1,9 @@
-// === PRODUCTS ===
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const productsList = document.getElementById('products-list');
-//   const items = productsList.querySelectorAll('.products-item');
-
-//   const numCopies = 10;
-//   for (let i = 0; i < numCopies - 1; i++) {
-//     items.forEach(item => {
-//       const clonedItem = item.cloneNode(true);
-//       productsList.appendChild(clonedItem);
-//     });
-//   }
-// });
-
-// === /PRODUCTS ===
-
 document.addEventListener('DOMContentLoaded', function () {
-  // Функція для клонування елементів списку
   function cloneListItems(listId, itemClass, numCopies) {
     const list = document.getElementById(listId);
-    const items = list.querySelectorAll(itemClass);
+    const items = list ? list.querySelectorAll(itemClass) : [];
+
+    if (!items.length) return;
 
     for (let i = 0; i < numCopies; i++) {
       items.forEach(item => {
@@ -30,8 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   cloneListItems('first-products-list', '.first-products-item', 10);
-
   cloneListItems('second-products-list', '.second-products-item', 10);
+  cloneListItems('running-list', '.running-item', 10);
 });
-
 
