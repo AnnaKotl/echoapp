ed54078c34d42289534cbfd00a0f3f60950d97d1
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-12-01 13:58:59 +0200

    TEAM var 1

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // clone PRODUCTS & running-line 📲 📱
   function cloneListItems(listId, itemClass, numCopies) {
     const list = document.getElementById(listId);
     const items = list ? list.querySelectorAll(itemClass) : [];
@@ -6,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (!items.length) return;
 
     for (let i = 0; i < numCopies; i++) {
-      items.forEach(item => {
+      items.forEach((item) => {
         const clonedItem = item.cloneNode(true);
         list.appendChild(clonedItem);
       });
@@ -16,4 +17,26 @@ document.addEventListener('DOMContentLoaded', function () {
   cloneListItems('first-products-list', '.first-products-item', 10);
   cloneListItems('second-products-list', '.second-products-item', 10);
   cloneListItems('running-list', '.running-item', 10);
+  //  📲 📱 /
+
+  // TEAM list slider 👩🏻‍💻👨🏼‍💻
+  function activateAdvantages() {
+    const advantages = document.querySelectorAll('.team-list .advantage');
+    if (!advantages.length) return;
+
+    let currentIndex = 0;
+
+    function highlightNextAdvantage() {
+      advantages.forEach((adv) => adv.classList.remove('active'));
+      advantages[currentIndex].classList.add('active');
+      currentIndex = (currentIndex + 1) % advantages.length;
+    }
+
+    setInterval(highlightNextAdvantage, 1500);
+
+    highlightNextAdvantage();
+  }
+
+  activateAdvantages();
+  // 👩🏻‍💻👨🏼‍💻 /
 });
