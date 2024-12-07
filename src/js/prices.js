41bab032f73a5b7715c14d81b1b954290c91b43f
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-12-07 17:03:34 +0200

    reset PRICES (data from DB)
    var1 - width: calc(50% - 20px)

--- src/js/prices.js
+++ src/js/prices.js
@@ -1,23 +1,45 @@
 // 💸 PRICES
 export function renderServices(services) {
-    const container = document.querySelector('.prices-list');
+  const container = document.querySelector('.prices-list');
 
-    if (!container) return;
+  if (!container) return;
 
-    const servicesHTML = services.map(service => {
-        return `
-        <li class="prices-item">
-          <div class="prices-item-container">
-            <h4 class="service-name">${service.name}</h4>
-            <p class="about-service">${service.description}</p>
-            <div class="order-container">
-              <p class="price">${service.price}</p>
-              <button type="button" class="order-btn">Order</button>
-            </div>
+  const servicesHTML = services.map(service => {
+
+    const descriptionHTML = service.description && service.description.length > 0
+      ? service.description.map(desc => `<p>${desc}</p>`).join('')
+      : '';
+
+    const featuresHTML = service.features.join(', ');
+
+    return `
+      <li class="prices-item">
+
+        <div class="prices-item-container">
+
+          <h4 class="service-name">${service.name}</h4>
+
+          ${descriptionHTML ? `<div class="service-description">${descriptionHTML}</div>` : ''}
+
+          <div class="about-service">
+            <span>The price of development includes:</span>
+            <p class="about-features">${featuresHTML}</p>
           </div>
-        </li>`;
-    }).join('');
+          
+          <div class="order-container">
 
-    container.innerHTML = servicesHTML;
+            <p class="price">${service.price}</p>
+
+            <button type="button" class="order-btn">Order</button>
+            
+          </div>
+
+        </div>
+        
+      </li>`;
+  }).join('');
+
+  container.innerHTML = servicesHTML;
 }
+
 // 💸 /
