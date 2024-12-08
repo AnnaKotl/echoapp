22be6e119b059a3e9e1e14c52716011cb8954a5c
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-12-08 20:26:52 +0200

    del npm uninstall @types/estree

--- frontend/src/js/config.js
+++ frontend/src/js/config.js
@@ -8,6 +8,6 @@
 
 const API_URL = process.env.NODE_ENV === 'production' 
 ? 'https://echoapp.onrender.com'  // URL для продакшн
-: 'http://localhost:5001';                // URL для локального
+: 'http://localhost:5001';        // URL для локального
 
 export default API_URL;
