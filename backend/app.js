23be9f2e19743b7419b07e78bb5e38e1bebc8874
Анна Котляр <123831146+AnnaKotl@users.noreply.github.com>
2024-12-26 19:16:54 +0200

    nev cors & app BACKEND

--- backend/app.js
+++ backend/app.js
@@ -20,12 +20,20 @@ connectDB();
 const app = express();
 keepAlive();
 
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+// console.log(`Environment: ${process.env.NODE_ENV}`);
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
-console.log(`Environment: ${process.env.NODE_ENV}`);
+console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(cors({ origin: 'https://echocode.netlify.app' }));
+} else {
+  app.use(cors(corsOptions));
+}
 
 app.use(logger(formatsLogger));
 // app.use(cors(corsOptions));                                        // - local 🩼
-app.use(cors({ origin: 'https://echocode.netlify.app' }));           // - production 🩼
+// app.use(cors({ origin: 'https://echocode.netlify.app' }));           // - production 🩼
 app.use(express.json());
 app.use(errorHandler);
 
--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,12 +1,31 @@
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log("CORS origin:", origin); 
+//     const allowedOrigins = [
+//       'https://echocode.netlify.app',
+//       'http://localhost:5173',
+//       'https://www.echocode.app',
+//     ];
+
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   optionsSuccessStatus: 200,
+// };
+
+const allowedOrigins = [
+  'https://echocode.netlify.app',
+  'http://localhost:5173',
+  'https://www.echocode.app',
+];
+
 const corsOptions = {
   origin: (origin, callback) => {
-    console.log("CORS origin:", origin); 
-    const allowedOrigins = [
-      'https://echocode.netlify.app',
-      'http://localhost:5173',
-      'https://www.echocode.app',
-    ];
-
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
