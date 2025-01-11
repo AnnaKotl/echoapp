6a06614baa4b90cf07edd8531181536ce4eca94c
Anna <kotlyaranya1771@gmail.com>
2025-01-11 15:28:35 +0200

    FIX modal
    add initializeMessageField for textarea
    
    maxlength
    
    autocomplete
    
    fix close-modal-btn position

--- frontend/src/js/pages/home/modal-form.js
+++ frontend/src/js/pages/home/modal-form.js
@@ -6,8 +6,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalForm = document.getElementById('modalForm');
     const openModalBtns = document.querySelectorAll('.js-open-modal');
     const container = document.querySelector('.prices-list');
+    
+    const adjustHeight = (field) => {
+        field.style.height = 'auto';
+        field.style.height = `${field.scrollHeight}px`;
+    };
+
+    const initializeMessageField = () => {
+        const messageField = document.getElementById('message');
+    
+        if (!messageField) return;
+    
+        let toastShown = false;
+    
+        messageField.addEventListener('input', (event) => {
+            const maxLength = 2000;
+            const currentLength = event.target.value.length;
+    
+            if (currentLength > maxLength) {
+                event.target.value = event.target.value.slice(0, maxLength);
+    
+                if (!toastShown) {
+                    showToast('Message exceeds the maximum length of 2000 characters. Excess text has been removed.', false);
+                    toastShown = true;
+                }
+            }
+    
+            adjustHeight(event.target);
+        });
+    
+        adjustHeight(messageField);
+    };    
+
     const openModal = (serviceName) => {
         modalForm.classList.add('modal-open');
+
+        modalForm.setAttribute('aria-hidden', 'false');
+        modalForm.setAttribute('role', 'dialog');
+        modalForm.setAttribute('aria-labelledby', 'modal-title');
+
         document.body.style.overflow = 'hidden';
         document.body.style.pointerEvents = 'none';
         modalForm.style.pointerEvents = 'auto';
@@ -15,42 +52,83 @@ document.addEventListener('DOMContentLoaded', () => {
         modalForm.innerHTML = `
   <div class="modal-content">
     <a class="close-modal-btn" id="close-button">
-        <svg class="close-modal-icon" width="10" height="10">
+        <svg class="close-modal-icon" width="30" height="30">
             <use href="${spriteUrl}#icon-cross"></use>
         </svg>
     </a>
+
+    <h2 id="modal-title" class="modal-title visually-hidden">Request a Service</h2>
 
     <form id="requestForm" class="form">
         <div class="form-group">
             <svg class="required-icon" width="1" height="1">
                 <use href="${spriteUrl}#chevron-right"></use>
             </svg>
-            <input type="text" id="name" name="name" class="form-input" placeholder="Your name" required>
+            <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                class="form-input" 
+                placeholder="Your name" 
+                autocomplete="name" 
+                required
+            >
         </div>
 
         <div class="form-group">
             <svg class="required-icon" width="1" height="1">
                 <use href="${spriteUrl}#chevron-right"></use>
             </svg>
-            <input type="text" id="mobileNumber" name="mobileNumber" class="form-input" placeholder="Your mobile number" required>
+            <input 
+                type="text" 
+                id="mobileNumber" 
+                name="mobileNumber" 
+                class="form-input" 
+                placeholder="Your mobile number" 
+                autocomplete="tel" 
+                required
+            >
         </div>
 
         <div class="form-group">
             <svg class="required-icon" width="1" height="1">
                 <use href="${spriteUrl}#chevron-right"></use>
             </svg>
-            <input type="email" id="email" name="email" class="form-input" placeholder="Your email" required>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                class="form-input" 
+                placeholder="Your email" 
+                autocomplete="email" 
+                required
+            >
         </div>
 
         <div class="form-group">
             <svg class="required-icon" width="1" height="1">
                 <use href="${spriteUrl}#chevron-right"></use>
             </svg>
-            <input type="text" id="country" name="country" class="form-input" placeholder="Your country" required>
+            <input 
+                type="text" 
+                id="country" 
+                name="country" 
+                class="form-input" 
+                placeholder="Your country" 
+                autocomplete="country-name" 
+                required
+            >
         </div>
 
         <div class="form-group">
-            <input type="text" id="socialNetwork" name="socialNetwork" class="form-input" placeholder="Social network for contact">
+            <input 
+            type="text" 
+            id="socialNetwork" 
+            name="socialNetwork" 
+            class="form-input" 
+            placeholder="Social network for contact"
+            autocomplete="off"
+            >
         </div>
 
         <p class="radio-title-label">Select a service:</p>
@@ -59,7 +137,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 .map(
                     service => `
                 <label for="service${service}" class="radio-label">
-                    <input id="service${service}" class="radio-input" type="radio" name="selectedService" value="${service}" ${service === 'Basic' ? 'checked' : ''} required />
+                    <input 
+                        id="service${service}" 
+                        class="radio-input" 
+                        type="radio" 
+                        name="selectedService" 
+                        value="${service}" ${service === 'Basic' ? 'checked' : ''} 
+                        required 
+                    />
                     <span class="radio-circle"></span>
                     ${service}
                 </label>`
@@ -68,7 +153,14 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
 
         <div class="form-group">
-            <textarea id="message" name="message" class="form-textarea" placeholder="Add a comment if you wish"></textarea>
+            <textarea 
+                id="message" 
+                name="message" 
+                class="form-textarea" 
+                placeholder="Add a comment if you wish"
+                autocomplete="off"
+                maxlength="2000"
+            ></textarea>
         </div>
 
         <button type="submit" class="form-button">Submit</button>
@@ -86,11 +178,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const closeModalBtn = document.getElementById('close-button');
         closeModalBtn.addEventListener('click', closeModal);
 
+        document.getElementById('name').focus();
+
         initializeModalForm(showToast);
+        initializeMessageField();
     };
 
     const closeModal = () => {
         modalForm.classList.remove('modal-open');
+        modalForm.setAttribute('aria-hidden', 'true');
         modalForm.innerHTML = '';
         document.body.style.overflow = '';
         document.body.style.pointerEvents = '';
@@ -122,3 +218,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
