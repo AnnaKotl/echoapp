2adabf4773b693faf213fda8a2a7e0309d7b428f
AnnaKotl <123831146+AnnaKotl@users.noreply.github.com>
2025-05-14 16:53:55 +0000

    Deploying to gh-pages from @ AnnaKotl/echoapp@19496c1f6b0d6d33d6152e80199ef3b1d7b93e8b 🚀

--- assets/admin-BgqWmI0W.js
+++ assets/admin-BgqWmI0W.js
@@ -1,4 +1,4 @@
-import"./main-D2CpY_tv.js";const r="/admin/requests",o=void 0;async function a(){try{const t=await fetch(`${r}?secret=${o}`);if(!t.ok)throw new Error("Failed to fetch admin requests");return await t.json()}catch(t){return console.error("Error fetching requests:",t),[]}}document.addEventListener("DOMContentLoaded",async()=>{if(new URLSearchParams(window.location.search).get("secret")===void 0){const s=document.querySelector(".requests-list"),n=await a();n.length>0?s.innerHTML=n.map(e=>`
+import"./main-BolQBBjN.js";const r="/admin/requests",o=void 0;async function a(){try{const t=await fetch(`${r}?secret=${o}`);if(!t.ok)throw new Error("Failed to fetch admin requests");return await t.json()}catch(t){return console.error("Error fetching requests:",t),[]}}document.addEventListener("DOMContentLoaded",async()=>{if(new URLSearchParams(window.location.search).get("secret")===void 0){const s=document.querySelector(".requests-list"),n=await a();n.length>0?s.innerHTML=n.map(e=>`
           <div class="request-item">
             <p><strong>Name:</strong> ${e.name}</p>
             <p><strong>Email:</strong> ${e.email}</p>
