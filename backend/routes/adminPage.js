4b39d5e461d37158a0f1b75f83b08cf6006d5591
AnnaK <kotlyaranya1771@gmail.com>
2025-08-19 15:58:34 +0300

    Update adminPage.js

--- backend/routes/adminPage.js
+++ backend/routes/adminPage.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
     }
   }
 
-  res.sendFile(path.join(__dirname, './frontend/dist/pages/admin.html'));
+  res.sendFile(path.join(__dirname, 'frontend/dist/pages/admin.html'));
   // res.sendFile(path.join(__dirname, '../../frontend/dist/pages/admin.html'));
 });
 
