296688a78cec8f08e73f2f0cd42df62d67f792c6
AnnaK <kotlyaranya1771@gmail.com>
2025-08-20 17:25:18 +0300

    Update config.js

--- frontend/src/js/helpers/config.js
+++ frontend/src/js/helpers/config.js
@@ -1,5 +1,12 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.NODE_ENV === 'production' 
+? 'https://echoapp.onrender.com'
+: 'http://localhost:5001';
+
 const ADMIN_PASSWORD = import.meta.env.VITE_ADMIN_PASSWORD;
 
-export default API_URL;
 export { ADMIN_PASSWORD };
+export default API_URL;
+
+// console.log("API URL:", API_URL);
+// console.log(import.meta.env);
+// console.log(import.meta.env.MODE);
