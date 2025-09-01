fe1875fb03934a4040bafe28ccf68046ae979547
AnnaK <kotlyaranya1771@gmail.com>
2025-09-01 17:56:05 +0300

    start portfolio main page

--- frontend/src/js/helpers/scroll-to-top.js
+++ frontend/src/js/helpers/scroll-to-top.js
@@ -1,37 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   const scrollButton = document.getElementById('scroll-button');
 
-  const toggleScrollButton = () => {
-    if (window.scrollY > 2000) {
-      scrollButton.classList.add('visible');
-      scrollButton.classList.remove('hidden');
-    } else {
-      scrollButton.classList.add('hidden');
-      scrollButton.classList.remove('visible');
-    }
-  };
+  if (scrollButton) {
+    const toggleScrollButton = () => {
+      if (window.scrollY > 1000) {
+        scrollButton.classList.add('visible');
+        scrollButton.classList.remove('hidden');
+      } else {
+        scrollButton.classList.add('hidden');
+        scrollButton.classList.remove('visible');
+      }
+    };
 
-  window.addEventListener('scroll', toggleScrollButton);
+    window.addEventListener('scroll', toggleScrollButton);
 
-  scrollButton.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
+    scrollButton.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     });
-  });
+  }
 
-  const scrollLink = document.querySelector('.js-scroll-link');
+  const scrollLinks = document.querySelectorAll('.js-scroll-link');
 
-  scrollLink.addEventListener('click', (event) => {
-    event.preventDefault();
+  if (scrollLinks.length > 0) {
+    scrollLinks.forEach((scrollLink) => {
+      scrollLink.addEventListener('click', (event) => {
+        event.preventDefault();
 
-    const targetSection = document.querySelector(scrollLink.getAttribute('href'));
+        const targetSelector = scrollLink.getAttribute('href');
+        const targetSection = document.querySelector(targetSelector);
 
-    window.scrollTo({
-      top: targetSection.offsetTop,
-      behavior: 'smooth',
+        if (targetSection) {
+          window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth',
+          });
+        }
+
+        scrollLink.blur();
+      });
     });
-
-    scrollLink.blur();
-  });
+  }
 });
