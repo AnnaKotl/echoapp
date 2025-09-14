0be31fe5ad6f3a26741832f93b00acd8a0622c1d
AnnaK <kotlyaranya1771@gmail.com>
2025-09-14 19:05:54 +0300

    lazy load CSS

--- frontend/src/js/pages/home/home.js
+++ frontend/src/js/pages/home/home.js
@@ -3,25 +3,9 @@ import '/js/pages/home/products';
 import '/js/pages/home/modal-form';
 import '/js/partials/preloader';
 import '/js/helpers/lazy-load-sections';
-import { lazyLoadCSS } from '/js/helpers/lazy-load-css.js';
 import { fetchServices } from '/js/api/api';
 import { cloneListItems } from '/js/pages/home/clone-items';
 import { activateAdvantages } from '/js/pages/home/team-slider';
-
-// ⚡ Lazy-load 
-window.addEventListener('load', () => {
-  lazyLoadCSS([
-    '/css/partials/home-products.css',
-    '/css/partials/home-prices.css',
-    '/css/partials/home-partnership.css',
-    '/css/partials/running-line.css',
-    '/css/partials/home-team.css',
-    '/css/partials/home-career.css',
-    '/css/partials/home-contacts.css',
-    '/css/partials/modal.css',
-    '/css/partials/footer.css'
-  ]);
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   // 📲 
--- frontend/src/js/pages/portfolio/portfolio.js
+++ frontend/src/js/pages/portfolio/portfolio.js
@@ -1,12 +1,1 @@
 import '/js/helpers/lazy-load-sections';
-import { lazyLoadCSS } from '/js/helpers/lazy-load-css.js';
-
-// ⚡ Lazy-load 
-window.addEventListener('load', () => {
-  lazyLoadCSS([
-    '/css/partials/footer.css',
-    '/css/partials/portfolio-cleaning-up.css',
-    '/css/partials/portfolio-e-commerce.css',
-    '/css/partials/portfolio-food-drink.css'
-  ]);
-});
