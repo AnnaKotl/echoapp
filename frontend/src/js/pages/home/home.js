d308a6c564159999a885857254456d0bf4df452a
AnnaK <kotlyaranya1771@gmail.com>
2025-09-14 17:55:30 +0300

    === portfolio css ===

--- frontend/src/js/pages/home/home.js
+++ frontend/src/js/pages/home/home.js
@@ -6,10 +6,33 @@ import { fetchServices } from '/js/api/api';
 import { cloneListItems } from '/js/pages/home/clone-items';
 import { activateAdvantages } from '/js/pages/home/team-slider';
 
-document.addEventListener('DOMContentLoaded', () => {  
+function loadNonCriticalCSS(href) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+const nonCriticalStyles = [
+  '/css/partials/home-products.css',
+  '/css/partials/home-prices.css',
+  '/css/partials/home-partnership.css',
+  '/css/partials/running-line.css',
+  '/css/partials/home-team.css',
+  '/css/partials/home-career.css',
+  '/css/partials/home-contacts.css',
+  '/css/partials/modal.css',
+  '/css/partials/footer.css'
+];
+
+window.addEventListener('load', () => {
+  nonCriticalStyles.forEach(loadNonCriticalCSS);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
   // 📲 
   cloneListItems('running-list', '.running-item', 10);
-  // 💸 
+  // 💸  
   fetchServices();
   // 👩🏻‍💻👨🏼‍💻 
   activateAdvantages();
--- frontend/src/js/pages/portfolio/portfolio.js
+++ frontend/src/js/pages/portfolio/portfolio.js
@@ -1,1 +1,17 @@
-import showToast from '/js/helpers/show-toast';
+function loadNonCriticalCSS(href) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+const nonCriticalStyles = [
+  '/css/partials/footer.css',
+  '/css/partials/portfolio-cleaning-up.css',
+  '/css/partials/portfolio-e-commerce.css',
+  '/css/partials/portfolio-food-drink.css'
+];
+
+window.addEventListener('load', () => {
+  nonCriticalStyles.forEach(loadNonCriticalCSS);
+});
